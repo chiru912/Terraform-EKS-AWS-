f2b2164b67927eb66d8f0e3f84e5d2352edf8eb9
NVVR <61036801+nvvr53@users.noreply.github.com>
2022-12-05 20:27:13 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "demo" {
-  name = "nvvr-cluster"
+  name = "nvvr-cluster-role"
 
   assume_role_policy = <<POLICY
 {
@@ -23,7 +23,7 @@ resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
 }
 
 resource "aws_eks_cluster" "demo" {
-  name     = "demo"
+  name     = "nvvr-cluster"
   role_arn = aws_iam_role.demo.arn
 
   vpc_config {
