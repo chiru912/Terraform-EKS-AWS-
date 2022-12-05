3efa56219a896968d7bcee9ccfbc8f84619739d3
NVVR <61036801+nvvr53@users.noreply.github.com>
2022-12-05 20:26:47 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "ap-south-1"
+}
+
+variable "access_key" {
+    default = "AKIAYYLBSKPPFRE7QHXJ"
+}
+
+variable "secret_key" {
+    default = "yrXxTMKKBkyoauobKUjS7nupyx1gbg4E1ZQT2DSc"
+}
